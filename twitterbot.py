9e99528cc65e382f7c081730bc69098d1639a440
Eoghan Martin <Eoghan Martin>
2017-02-05 19:34:20 -0500

    task 3

--- twitterbot.py
+++ twitterbot.py
@@ -2,51 +2,39 @@
  
 import tweepy, time, sys, json, pdb
 
+#import config file, not included in git repo
 filename=open('config.txt','r')
 keys=filename.readlines()
 filename.close()
  
+#Add custom keys here
 CONSUMER_KEY = str(keys[1].replace(' ', '')[:-1])
 CONSUMER_SECRET = str(keys[3].replace(' ', '')[:-1])
 ACCESS_KEY = str(keys[5].replace(' ', '')[:-1])
 ACCESS_SECRET = str(keys[7])
-pdb.set_trace()
 
-argfile = str(sys.argv[1])
+#pdb.set_trace()
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
- 
-class StdOutListener(tweepy.StreamListener):
-    def on_data(self, data):
-        decoded = json.loads(data)
-        print '@%s: %s' % (decoded['user']['screen_name'], decoded['text'].encode('ascii', 'ignore'))
-        print ''
-        return True
-
-    def on_error(self, status):
-        print status
 
 if __name__ == '__main__':
-	l = StdOutListener()
-
-	filename=open(argfile,'r')
-	f=filename.readlines()
+	filename = open("user_IDs.txt",'r')
+	ids = filename.readlines()
 	filename.close()
 
-	for line in f:
+	task1 = open('task1.txt', 'w')
+	task2 = open('task2.txt', 'w')
+	for line in ids:
 		account_info = api.lookup_users(user_ids=[line])
 		for user in account_info:
-
-			f = open('task1.txt', 'w')
-			f.write("Screen Name: %s\n" % (str(user.screen_name)))
-			f.write("User Name: %s\n" % (str((user.name))))
-			f.write("Account Description: %s\n" % str(user.description))
-			f.write("Followers Count: %s\n" % (str(user.followers_count)))
-			f.write("Statuses Count: %s\n" % (str(user.statuses_count)))
-			f.write("Account URL: %s\n\n" % (str(user.url)))
-			f.close()
+			task1.write("Screen Name: %s\n" % (str(user.screen_name)))
+			task1.write("User Name: %s\n" % (str((user.name))))
+			task1.write("Account Description: %s\n" % str(user.description))
+			task1.write("Followers Count: %s\n" % (str(user.followers_count)))
+			task1.write("Statuses Count: %s\n" % (str(user.statuses_count)))
+			task1.write("Account URL: %s\n\n" % (str(user.url)))
 
 			print "Screen Name: %s" % (user.screen_name)
 			print "User Name: %s" % (user.name)
@@ -55,22 +43,31 @@ if __name__ == '__main__':
 			print "Statuses Count: %s" % (str(user.statuses_count))
 			print "Account URL: %s\n" % (str(user.url))
 			
-			f = open('task2.txt', 'w')
-			f.write("Followers for @%s:" % (user.screen_name))
-			print "Followers for @%s:" % (user.screen_name)
-			for follower in tweepy.Cursor(api.followers, screen_name=user.screen_name, count=200).items():
+			task2.write("Followers for @%s:" % (user.screen_name))
+			print "Followers for @%s:\n" % (user.screen_name)
+			followers = tweepy.Cursor(api.followers, screen_name=user.screen_name, count=20).pages()
+			page = next(followers)
+			for follower in page:
 				print "%s (%s)" % (follower.screen_name.encode('utf-8').strip(), follower.location.encode('utf-8').strip())
-				f.write("%s (%s)\n" % (follower.screen_name.encode('utf-8').strip(), follower.location.encode('utf-8').strip()))
-			f.write("Friends for @%s:" % (user.screen_name))
+				task2.write("%s (%s)\n" % (follower.screen_name.encode('utf-8').strip(), follower.location.encode('utf-8').strip()))
+			task2.write("Friends for @%s:\n" % (user.screen_name))
 			print "\nFriends:"
-			for friend in tweepy.Cursor(api.friends, screen_name=user.screen_name, count=200).items():
-				f.write("%s (%s)\n" % (friend.screen_name.encode('utf-8').strip(), friend.location.encode('utf-8').strip()))
-				print '%s (%s)' % (friend.screen_name.encode('utf-8').strip(), friend.location.encode('utf-8').strip())
-			f.close()
+			friends = tweepy.Cursor(api.friends, screen_name=user.screen_name, count=20).pages()
+			page = next(friends)
+			for friend in page:
+				print "%s (%s)" % (friend.screen_name.encode('utf-8').strip(), friend.location.encode('utf-8').strip())
+				task2.write("%s (%s)\n" % (friend.screen_name.encode('utf-8').strip(), friend.location.encode('utf-8').strip()))
+	task2.close()
+	task1.close()
 
-		time.sleep(60*10)
+	print "\nWeather keyword:\n"
 
-	print "\nShowing all new tweets for #programming:"
+	for tweet in tweepy.Cursor(api.search, q="weather", result_type="recent").items(50):
+		#pdb.set_trace()
+		print tweet.text.encode('utf-8').strip()
+		break
 
-	stream = tweepy.Stream(auth, l)
-	stream.filter(track=['programming'])
+	for tweet in tweepy.Cursor(api.search, geocode="-22.9122,-43.2302,1km", result_type="recent").items(50):
+		#pdb.set_trace()
+		print tweet.text.encode('utf-8').strip()#[-86.33,41.63,-86.20,41.74]
+		break
