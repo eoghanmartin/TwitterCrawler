3acb93a62ee0cb2364c845d5c1cf26003451c718
Eoghan Martin <Eoghan Martin>
2017-03-06 12:49:27 -0500

    new folder setup

--- Assignment2/main.py
+++ Assignment2/main.py
@@ -6,4 +6,4 @@ if __name__ == '__main__':
 	k_means = KMeans('InitialSeeds.txt', 'Tweets.json')
 	k_means.generateClusters()
 	k_means.writeClustersToFile('clusters.json')
-	k_means.checkResults('CorrectResults.txt')
+	k_means.checkResults('CorrectClusteringResults.txt')
